17865c412b0f38ac9bec47ad65a8128b5c49eda5
Ivan Molodetskikh <yalterz@gmail.com>
2016-08-09 11:45:04 +0300

    Added cl_autowepswitch.

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -398,6 +398,9 @@ void CHud :: Init( void )
 	CVAR_CREATE( "hud_classautokill", "1", FCVAR_ARCHIVE | FCVAR_USERINFO );		// controls whether or not to suicide immediately on TF class switch
 	CVAR_CREATE( "hud_takesshots", "0", FCVAR_ARCHIVE );		// controls whether or not to automatically take screenshots at the end of a round
 
+	// Implemented server-side, needs to be registered client-side.
+	CVAR_CREATE( "cl_autowepswitch", "1", FCVAR_ARCHIVE | FCVAR_USERINFO | FCVAR_CLIENTDLL );
+
 	m_iLogo = 0;
 	m_iFOV = 0;
 
