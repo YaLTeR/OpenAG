23a2ffed740b5df4f781e1cbeea256c0e1a022db
megasausageking <azruio.qw@gmail.com>
2017-01-02 20:54:06 +0100

    added features for cl_scores

--- cl_dll/cl_scores.cpp
+++ cl_dll/cl_scores.cpp
@@ -1,14 +1,13 @@
 // +msk
-#include <iostream>
-#include <algorithm> // reverse,sort,min
-#include <vector>
-#include <string>
-#include <sstream>
+#include <iostream> // debug
+#include <algorithm> // std::reverse,std::sort,std::min
+#include <vector> // std::vector
+#include <string> // std::string
+#include <sstream> // std::ostringstream
 #include "hud.h"
 #include "cl_util.h"
 #include "vgui_TeamFortressViewport.h"
 #include <cctype> // std::isdigit
-
 
 extern int iNumberOfTeamColors; // in vgui_TeamFortressViewport.cpp
 extern int iTeamColors[5][3]; // in vgui_TeamFortressViewport.cpp
@@ -18,25 +17,23 @@ extern team_info_t g_TeamInfo[MAX_TEAMS + 1];
 
 namespace ScoreTabs
 {
-
+  enum class DrawType{AUTO,MANUAL, NO_UNIFORM}; 
   int ScoreList::Init()
   {
     m_iFlags = 0;
 
     gHUD.AddHudElem(this);
 
-    cl_scores=CVAR_CREATE("cl_scores","0",FCVAR_ARCHIVE); // msk: 0, 1 , 2 , ...
-    cl_scores_x=CVAR_CREATE("cl_scores_x","0",FCVAR_ARCHIVE); // msk: posi
+    cl_scores=CVAR_CREATE("cl_scores","0",FCVAR_ARCHIVE); 
+    cl_scores_x=CVAR_CREATE("cl_scores_x","0",FCVAR_ARCHIVE);
     cl_scores_y=CVAR_CREATE("cl_scores_y","0",FCVAR_ARCHIVE);
 
-    cl_scores_width=CVAR_CREATE("cl_scores_width","0",FCVAR_ARCHIVE); // non uniform if zéro
-    cl_scores_brightness=CVAR_CREATE("cl_scores_brightness","30",FCVAR_ARCHIVE); // ew
-    cl_scores_hpad=CVAR_CREATE("cl_scores_hpad","5",FCVAR_ARCHIVE); // ew
-    cl_scores_vpad=CVAR_CREATE("cl_scores_vpad","0",FCVAR_ARCHIVE); // ew
+    cl_scores_width=CVAR_CREATE("cl_scores_width","0",FCVAR_ARCHIVE); 
+    cl_scores_brightness=CVAR_CREATE("cl_scores_brightness","30",FCVAR_ARCHIVE);
+    cl_scores_hpad=CVAR_CREATE("cl_scores_hpad","5",FCVAR_ARCHIVE); // horizontal padding
+    cl_scores_vpad=CVAR_CREATE("cl_scores_vpad","0",FCVAR_ARCHIVE); // vertical padding
     return 0;
   }
-
-  
 
   int ScoreList::VidInit()
   {
@@ -68,72 +65,107 @@ namespace ScoreTabs
     return tags_count;
   }
 
+  static int longest_line_chars(const std::vector<tab_t> & t)
+  {
+    int long_size=0;
+    for(auto i : t)
+      {
+	std::ostringstream oss;
+	oss.str("");
+	oss.clear();
+	std::string curline;
+	curline.clear();
+
+	oss << i.score << "   " << i.name;
+	curline = oss.str();
+
+	int size = curline.size() - 2*count_color_tags(curline);
+	if(size>long_size)
+	  long_size = size;
+      }
+    return long_size;
+  }
+
   void ScoreList::DrawSortedTabs()
   {
-    bool bTeamplay = gHUD.m_Teamplay;
-    int brightness = static_cast<int>(cl_scores_brightness->value);
-    int width = static_cast<int>(cl_scores_width->value);
+    const bool bTeamplay = gHUD.m_Teamplay;
+    const int hpaddingsize = static_cast<int>(cl_scores_hpad->value);
+    const int vpaddingsize = static_cast<int>(cl_scores_vpad->value);
+    const int brightness = static_cast<int>(cl_scores_brightness->value);
+
+    DrawType drawing_mode {DrawType::NO_UNIFORM};
+
+    int width = 0;
+    if(!strncmp(cl_scores_width->string, "auto", 8))
+      drawing_mode = DrawType::AUTO; // width will be determined automatically
+    else if(cl_scores_width->value == 0)
+      drawing_mode = DrawType::NO_UNIFORM;  // disregard width, cells size not uniform
+    else
+      {
+	drawing_mode = DrawType::MANUAL; // user set width (clamp or extend)
+	width = static_cast<int>(cl_scores_width->value);
+      }
     
-    
-    std::string str;
-    std::vector<char> cstr; // drawhudstring want c-string so give a c-string to drawhudstring ok
-    int count = 0;
-    int depth = static_cast<int>(cl_scores->value);
-    int hpaddingsize = static_cast<int>(cl_scores_hpad->value);
-    int vpaddingsize = static_cast<int>(cl_scores_vpad->value);
+    std::string current_line; // will store each tabs line
+    std::vector<char> cstr; //  c-string for drawhudstring 
+    int depth = static_cast<int>(cl_scores->value); // how many items should we draw
+    int count = 0; // how many have we drawn   
+    int longest_line = 0; // longest (modulo colortags) line size of tabs 
+
     if(tabs.size() != 0)
       {
-	std::reverse(tabs.begin(),tabs.end()); // best team/player first
-	
+	std::reverse(tabs.begin(),tabs.end()); // highest score to lowest score order (top->down)
+
+	if(drawing_mode == DrawType::AUTO)
+	  longest_line = longest_line_chars(tabs);
+
 	for(auto it = tabs.begin(); it != tabs.end(); ++it)
 	  {
-	    int caracteres =  0;
+	    int characters =  0; // how many characters will be drawn
 	    std::ostringstream oss;
 	    oss.str("");
 	    oss.clear();
-	    str.clear();
+	    current_line.clear();
 	    cstr.clear();
 
-	    tab_t cur = *it;
+	    tab_t cur = *it; 
 	    oss << cur.score << "   " << cur.name;
-	    str=oss.str();
+	    current_line=oss.str();
 
-	    if(width == 0)
-	      caracteres = str.size() - 2*count_color_tags(str);
+	    if(drawing_mode == DrawType::NO_UNIFORM)
+	      characters = current_line.size() - 2*count_color_tags(current_line);
+	    else if(drawing_mode == DrawType::MANUAL)
+	      characters = width;
 	    else
-	      caracteres = width;
+	      characters = longest_line;
 
-	    int copied = 0;
-
-	    int i = 0;
+	    int copied = 0; // how many characters (modulo colortags) have we copied
+	    int i = 0; 
 	    cstr=std::vector<char>{};
-	    while(copied < caracteres) // fill cstring until no space left. ignore tags 
+	    int cursize = current_line.size();
+
+	    while((copied < characters) && (i < cursize) ) // fill cstring until no space left. ignore tags 
 	      {
-		if(str[i] == '^' && ((str[i+1] <= '9') && (str[i+1] >= '0'))) // quest: store this info in count_color_tags
+		if(current_line[i] == '^' && ((current_line[i+1] <= '9') && (current_line[i+1] >= '0'))) 
 		  {
-		    cstr.push_back(str[i]);
-		    cstr.push_back(str[i+1]);
+		    cstr.push_back(current_line[i]);
+		    cstr.push_back(current_line[i+1]);
 		    i = i + 2;
 		  }
 		else
 		  {
 		    copied = copied + 1;
-		    cstr.push_back(str[i]);
+		    cstr.push_back(current_line[i]);
 		    i = i + 1;
 		  }
 
 	      }
 	    cstr.push_back('\0');
-	    
-	    /* // old code men)
-	       cstr= std::vector<char> {str.begin(), str.end()};
-	       cstr.push_back('\0'); 
-	    */
 
-	    int largeur = gHUD.m_scrinfo.charWidths['a'];
-	    int hauteurcarac=gHUD.m_scrinfo.iCharHeight;
+	    int length_of_a_character = gHUD.m_scrinfo.charWidths['a']; // use width of 'a' as a reference for all characters
+	    int height_of_a_character=gHUD.m_scrinfo.iCharHeight;
 	    float x = cl_scores_x->value;
-	    float y = cl_scores_y->value + count*(hauteurcarac + 2*vpaddingsize);
+	    float y = cl_scores_y->value + count*(height_of_a_character + 2*vpaddingsize);
 	    
 	    int r,g,b;
 	    if(bTeamplay)
@@ -145,11 +177,11 @@ namespace ScoreTabs
 	    else
 	      UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
 	    
-	    gEngfuncs.pfnFillRGBA(x - hpaddingsize,y - vpaddingsize,(caracteres-1)*largeur + 2*hpaddingsize,hauteurcarac + 2*vpaddingsize,r,g,b,brightness);
+	    gEngfuncs.pfnFillRGBA(x - hpaddingsize,y - vpaddingsize,(characters-1)*length_of_a_character + 2*hpaddingsize,height_of_a_character + 2*vpaddingsize,r,g,b,brightness);
 	    ScaleColors(r,g,b,135);
 	    gHUD.DrawHudStringWithColorTags(x,y,&cstr[0],r,g,b);
 
-	    count=count+1;
+	    count=count+1; // don't draw too many
 	    if(count>(depth-1))
 	      return;
 	  }
@@ -159,7 +191,6 @@ namespace ScoreTabs
 
   int ScoreList::Draw(float flTime)
   {
-    // GREY QUEST: dont do anything if cl scores value is zero
     if(cl_scores->value != 0) {
       bool bTeamplay = gHUD.m_Teamplay;
       tabs.clear();
@@ -176,9 +207,9 @@ namespace ScoreTabs
 		continue;
 	      else
 		{
-		  name=std::string(g_TeamInfo[i].name); // do not strip color tags now
+		  name=std::string(g_TeamInfo[i].name); 
 		  frags=g_TeamInfo[i].frags;
-		  teamnum=g_PlayerExtraInfo[i].teamnumber; // for team color)
+		  teamnum=g_PlayerExtraInfo[i].teamnumber;
 		  tabs.push_back({frags, teamnum, name});
 		}
 	    }
