e22e11901f719e4469330e9a5df220d5878fd031
dxtzero <dxtzero@gmail.com>
2017-05-02 02:43:38 -0600

    Add CVARs for crosshair lines

--- cl_dll/hud_crosshairs.h
+++ cl_dll/hud_crosshairs.h
@@ -12,6 +12,10 @@ class CHudCrosshairs : public CHudBase
 	cvar_t* cl_cross_outline;
 	cvar_t* cl_cross_circle_radius;
 	cvar_t* cl_cross_dot_size;
+	cvar_t* cl_cross_top_line;
+	cvar_t* cl_cross_bottom_line;
+	cvar_t* cl_cross_left_line;
+	cvar_t* cl_cross_right_line;
 
 	float old_circle_radius;
 	std::vector<Vector2D> circle_points;
