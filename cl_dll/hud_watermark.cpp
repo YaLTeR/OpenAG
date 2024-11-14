1b33f0bee2cbdf53c13159697d9c8fc901a59651
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-11-14 10:16:54 +0000

    Add hud_watermark command (#191)
    hud_watermark <0|1> (1 by default)
    
    Controls whether the watermark & server-settings will display or not. The update disclaimer will display regardless, but only for 5 seconds if hud_watermark is set to 0.
    
    Co-authored-by: Ivan Molodetskikh <yalterz@gmail.com>

--- cl_dll/hud_watermark.cpp
+++ cl_dll/hud_watermark.cpp
@@ -3,6 +3,8 @@
 #include "parsemsg.h"
 #include "update_checker.h"
 
+extern cvar_t* hud_watermark;
+
 int CHudWatermark::Init()
 {
 	m_iFlags = 0;
@@ -22,9 +24,13 @@ int CHudWatermark::VidInit()
 
 int CHudWatermark::Draw(float time)
 {
-	if (refresh_draw_until || (draw_until > gHUD.m_flTime + 15.0f)) {
+	float duration = 15.0f;
+	if (hud_watermark->value != 1)
+		duration = 5.0f;
+		
+	if (refresh_draw_until || (draw_until > gHUD.m_flTime + duration)) {
 		refresh_draw_until = false;
-		draw_until = gHUD.m_flTime + 15.0f;
+		draw_until = gHUD.m_flTime + duration;
 	}
 
 	if (gHUD.m_flTime >= draw_until) {
@@ -35,8 +41,10 @@ int CHudWatermark::Draw(float time)
 	int r, g, b;
 	UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
 
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight, "OpenAG client build " __DATE__, r, g, b);
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "j.mp/OpenAG", r, g, b);
+	if (hud_watermark->value != 0) {
+		gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight, "OpenAG client build " __DATE__, r, g, b);
+		gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "j.mp/OpenAG", r, g, b);
+	}
 
 	if (update_is_available)
 		gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight / 2 * 7, "An update is available.", r, g, b);
